a7391ea290de4338431d4e6dc776d3dba1110fab
Hussein Dowi <56203425+husseindowi11@users.noreply.github.com>
2024-04-22 14:49:01 +0300

    test

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -27,7 +27,7 @@ const OrdersPage = () => {
 
   const mutation = useMutation({
     mutationFn: ({ id, status }: { id: string; status: string }) => {
-      return fetch(`http://localhost:3000/api/orders/${id}`, {
+      return fetch(`https://chromolitebite.netlify.app/api/orders/${id}`, {
         method:"PUT",
         headers: {
           "Content-Type": "application/json",
